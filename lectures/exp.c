7e5713460310f4c4ac2c80d992f4f25e5d10197b
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-06 14:45:35 +0900

    Create exp.c

---
+++ lectures/exp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+int main(){
+    solution({1,2,3,4,5,6,7,8,9,0}, 10);
+    return 0;
+}
+// arr_len은 배열 arr의 길이입니다.
+double solution(int arr[], size_t arr_len) {
+    double answer = 0;
+    printf('%d', arr_len);
+    return answer;
+}
