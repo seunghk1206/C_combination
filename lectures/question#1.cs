1e12864f4adea08a15ab214b59d2cc65223493ff
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-23 22:37:49 +0900

    Create question#1.cs

---
+++ lectures/question#1.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string solution(int a, int b) {
+        int SumODays, i;
+        SumODays = 0
+        int[] dateArr = {31,29,31,30,31,30,31,31,30,31,30,31};
+        string[] dayArr = {"FRI, SAT, SUN, MON, TUE, WED, THU"};
+        for(i = 0; i < a;i++){
+            SumODays += dateArr[i];
+        }
+        SumODays += b;
+        return dayArr[SumODays%7];
+    }
+}
