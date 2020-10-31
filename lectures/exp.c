c92ffa354fe68f3ac7802c881eda26da687be1da
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-31 12:56:06 +0900

    Update exp.c

--- lectures/exp.c
+++ lectures/exp.c
@@ -2,7 +2,15 @@
 #include <stdbool.h>
 #include <stdlib.h>
 int main(){
-    solution({1,2,3,4,5,6,7,8,9,0}, 10);
+    char a = "a";
+    return 0;
+}
+
+
+/*
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,0};
+    solution(arr, 10);
     return 0;
 }
 // arr_len은 배열 arr의 길이입니다.
@@ -11,3 +19,4 @@ double solution(int arr[], size_t arr_len) {
     printf('%d', arr_len);
     return answer;
 }
+*/
