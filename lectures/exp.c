5384bbac88397cc1a13675628c3b982b31bcb16e
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-11-04 11:16:32 +0900

    Update exp.c

--- lectures/exp.c
+++ lectures/exp.c
@@ -9,15 +9,12 @@ char* solution(int a, int b) {
     int dateArr[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
     char* dayArr[7] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
     char* answer;
-    // 리턴할 값은 메모리를 동적 할당해주세요.
     for(i = 0; i < a-1;i++){
         SumODays += dateArr[i];
     }
     SumODays += b;
     SumODays -= 1;
-    answer = (char*)malloc(10);
-    printf("%d\n", SumODays);
-    strcpy(answer, dayArr[SumODays%7]);
+    answer = dayArr[SumODays%7];
     return answer;
 }
 char* main(){
