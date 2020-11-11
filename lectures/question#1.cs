835cef91c7e3605d62b3bb6c6927a881b3e20743
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-11-11 11:03:27 +0900

    Update question#1.cs

--- lectures/question#1.cs
+++ lectures/question#1.cs
@@ -2,7 +2,7 @@ using System;
 public class Solution {
     public string solution(int a, int b) {
         int SumODays, i;
-        SumODays = 0
+        SumODays = 0;
         int[] dateArr = {31,29,31,30,31,30,31,31,30,31,30,31};
         string[] dayArr = {"FRI, SAT, SUN, MON, TUE, WED, THU"};
         for(i = 0; i < a;i++){
