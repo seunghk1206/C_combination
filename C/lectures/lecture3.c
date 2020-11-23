b098fe7766aa4448f5f4ed3ab3eba8d136ad38bb
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-11-23 22:39:43 +0900

    Create lecture3.c

---
+++ C/lectures/lecture3.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+int main(){
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    
+    printf("3rd value of this arr: %d \n", arr[20]);
+    /*
+    int a,b;
+    float c,d;
+
+    printf("input two nums: ");
+    scanf("%d %d", &a, &b);
+
+    c = a / b;
+    d = (float)a/b;
+
+    printf("ratio of the two nums: %f, %f", c, d);
+    */
+    /*
+    int a;
+    double b;
+
+    b = 2.5;
+    a = b;
+    */
+    //or you can use:
+    /*
+    int a;
+    double b;
+    a = 2;
+
+    b = (double)a;//casting
+    printf("%d", b);
+    */
+    return 0;
+}
+/*
+int main(){
+    int input;
+
+    printf("Read Alphabet!\n");
+    printf("Alphabet: \n");
+
+    scanf("%c", &input);
+
+    switch(input){
+        case 'a':
+            printf("Um~~~ Delicious \n");
+            break;
+        case 'b':
+            printf("Um~~~ So Fresh! \n");
+            break;
+        case 'c':
+            printf("zzzzzz.. \n");
+            break;
+        default:
+            printf("I do not understand what you are saying!! \n");
+            break;
+    }
+}
+*/
+/*
+int main(){
+    int input;
+
+    printf("My pet upgrade\n");
+    printf("Tell me what to do! \n");
+    printf("1. Feed\n");
+    printf("2. Wash\n");
+    printf("3. Sleep\n");
+
+    scanf("%d", &input)
+
+    switch(input){
+        case 1:
+            printf("Um~~~ Delicious \n");
+            break;
+        case 2:
+            printf("Um~~~ So Fresh! \n");
+            break;
+        case 3:
+            printf("zzzzzz.. \n");
+            break;
+        default:
+            printf("I do not understand what you are saying!! \n");
+            break;
+    }
+}
+*/
