0485294246dd402680d6f166668fd8020e4bb4c8
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-11-23 22:39:13 +0900

    Create pointer_exp.c

---
+++ C/lectures/pointer_exp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <C:\Users\skkim\OneDrive\Documents\GitHub\C_combination\C\myFunctions\myFunc.c>
+int main(){
+    char *p = "hello";
+    printf("%s \n", p);
+}
+/*
+int main(){
+    char a;
+    char *p;
+    p = &a;
+    *p = "hello";//does not work!
+    printf("%c, %c \n", a, *p);
+}
+*/
